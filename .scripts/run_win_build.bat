93a0180ebbe6a9813e99276ffe7263ea0771eb08
Isuru Fernando <isuruf@gmail.com>
2026-07-16 16:04:25 -0700

    Update run_win_build.bat

--- .scripts/run_win_build.bat
+++ .scripts/run_win_build.bat
@@ -12,6 +12,8 @@
 
 setlocal enableextensions enabledelayedexpansion
 
+@echo ON
+
 FOR %%A IN ("%~dp0.") DO SET "REPO_ROOT=%%~dpA"
 if "%MINIFORGE_HOME%"=="" set "MINIFORGE_HOME=%USERPROFILE%\Miniforge3"
 :: Remove trailing backslash, if present
